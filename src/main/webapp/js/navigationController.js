ae0624f1334b49339d0c40a326e88589b1bf28e6
Lasantha Ekanayake <mdslasantha@gmail.com>
2016-05-28 16:12:07 +0530

    navigationController.js added

--- src/main/webapp/js/navigationController.js
+++ src/main/webapp/js/navigationController.js
@@ -1,3 +1,47 @@
 /**
  * Created by Lasantha on 28-May-16.
  */
+
+app.controller('globalController', function($scope, $timeout, $mdSidenav, $log){
+    $scope.toggleLeft = buildToggler('left');
+
+    $scope.isOpenLeft = function(){
+        return $mdSidenav('left').isOpen();
+    };
+
+    function buildToggler(navID) {
+        return function() {
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+        }
+    }
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+        var timer;
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+                timer = undefined;
+                func.apply(context, args);
+            }, wait || 10);
+        };
+    }
+});
+
+app.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+        $mdSidenav('left').close()
+            .then(function () {
+                $log.debug("close RIGHT is done");
+            });
+    };
+});
