e94461d0aaa9f1f37fb6968e720329ced5bc6fd8
Lasantha Ekanayake <mdslasantha@gmail.com>
2016-05-28 15:22:21 +0530

    Test SideNav added

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -37,3 +37,49 @@ app.controller("helloWorld", function($scope, $mdDialog, $mdMedia, hellofy) {
 });
 
 app.controller('ip-mainmenu-controller', ip_mainmenu_controller);
+
+// Lasantha test
+
+app.controller('globalController', function($scope, $timeout, $mdSidenav, $log){
+    $scope.toggleLeft = buildToggler('left');
+
+    $scope.isOpenLeft = function(){
+        return $mdSidenav('left').isOpen();
+    };
+
+    function buildToggler(navID) {
+        return function() {
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+        }
+    }
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+        var timer;
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+                timer = undefined;
+                func.apply(context, args);
+            }, wait || 10);
+        };
+    }
+});
+
+app.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+        $mdSidenav('left').close()
+            .then(function () {
+                $log.debug("close RIGHT is done");
+            });
+    };
+});
