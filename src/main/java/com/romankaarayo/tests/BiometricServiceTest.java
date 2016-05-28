bf195f041aad01f76bf1c01e0991fe608a921b17
KRVPerera <rukshan.viduranga@gmail.com>
2016-05-28 15:53:18 +0530

    Test images
    Signed-off-by: KRVPerera <rukshan.viduranga@gmail.com>

--- src/main/java/com/romankaarayo/tests/BiometricServiceTest.java
+++ src/main/java/com/romankaarayo/tests/BiometricServiceTest.java
@@ -1,10 +1,12 @@
 package com.romankaarayo.tests;
-
+import junit.framework.*;
 import static org.junit.Assert.*;
 
 /**
  * Created by krv on 5/28/2016.
  */
-public class BiometricServiceTest {
-
+public class BiometricServiceTest extends TestCase {
+    public void testEnrollment() throws Exception {
+        
+    }
 }
